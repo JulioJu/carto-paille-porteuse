359f13a1a1c74e94fd51de7dc61f261dd5c7aafe
JulioJu <11527844+JulioJu@users.noreply.github.com>
2022-08-30 14:35:00 +0200

    feat: b4a Routing & Single Page Application

---
+++ cloud/app.js
@@ -0,0 +1,44 @@
+/**
+ * Routing & Single Page Application
+ * https://help.back4app.com/hc/en-us/articles/360031634031-Routing-Single-Page-Application-
+ */
+// eslint-disable-next-line no-undef
+const path = require("path");
+const unRoutedPaths = [
+  "apps",
+  "batch",
+  "requestPasswordReset",
+  "files",
+  "login",
+  "logout",
+  "user",
+  "users",
+  "Roles",
+  "parse",
+  "schemas",
+  "functions",
+  "classes",
+  "aggregate",
+  "cloud_code",
+  "config",
+  "hooks",
+  "push_audiences",
+  "installations",
+  "push",
+  "sessions",
+  "events",
+  "jobs",
+  "export_progress",
+  "export_data",
+  "graphql",
+];
+// eslint-disable-next-line no-undef
+app.use((req, res, next) => {
+  const pathParts = req.path.split("/").filter((p) => p !== "");
+  if (req.path.indexOf(".") > 0 || unRoutedPaths.includes(pathParts[0])) {
+    next();
+  } else {
+    // eslint-disable-next-line no-undef
+    res.sendFile(path.join(`${__dirname}/public/index.html`));
+  }
+});
