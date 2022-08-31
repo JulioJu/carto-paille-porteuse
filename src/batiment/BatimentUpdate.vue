a6235f0cd562a8dbc97df3d30f00cff8538328a7
JulioJu <11527844+JulioJu@users.noreply.github.com>
2022-08-31 11:48:52 +0200

    Revert "refactor: change BatimentUpdate.vue from Composition API to traditional syntax"
    This reverts commit 331ccccad1c6b49b330bf9bf5deff5ed34703218.

--- src/batiment/BatimentUpdate.vue
+++ src/batiment/BatimentUpdate.vue
@@ -1,51 +1,16 @@
-<template>
-  <h2>Créer ou</h2>
-  {{ created }}
-  {{ batiment.latitude }}
-  {{ batiment.longitude }}
-</template>
+<template>Create or update form {{ batiment.latitutde }}</template>
 <script lang="ts">
-import { defineComponent, type ComponentPublicInstance } from "vue";
+import { defineComponent, reactive, type ComponentPublicInstance } from "vue";
 
 interface IInstance extends ComponentPublicInstance {
-  created: string;
   retrieveBatiment(id: string): void;
-  setBatiment({ latitude }: { latitude: number; longitude: number }): void;
+  setBatiment({ latitude }: { latitude: string }): void;
 }
 
-type batimentUpdateVue = {
-  created: string;
-  batiment: { latitude?: number; longitude?: number };
-};
-
 export default defineComponent({
-  data: (): batimentUpdateVue => {
-    return {
-      created: "",
-      batiment: {},
-    };
-  },
-  methods: {
-    retrieveBatiment(id: string) {
-      console.debug(id);
-    },
-    setBatiment({
-      latitude,
-      longitude,
-    }: {
-      latitude: number;
-      longitude: number;
-    }) {
-      this.batiment.latitude = latitude;
-      this.batiment.longitude = longitude;
-      console.debug("vue-route::from::", this.batiment);
-      console.debug("vue-route::from::", this.batiment.latitude);
-    },
-  },
   beforeRouteEnter(to, _, next) {
     next((vm) => {
       const instance = vm as IInstance;
-      instance.created = "titi";
       if (to.params.batimentId) {
         // For instance http://127.0.0.1:5173/batiment/2961/edit
         instance.retrieveBatiment(to.params.batimentId as string);
@@ -62,12 +27,29 @@ export default defineComponent({
         Number(to.query.long) <= 90
       ) {
         console.debug(to.query.lat, to.query.long, "yoyo");
-        instance.setBatiment({
-          latitude: Number(to.query.long.toString()),
-          longitude: Number(to.query.lat.toString()),
-        });
+        instance.setBatiment({ latitude: to.query.lat.toString() });
       }
     });
   },
 });
+</script>
+<script setup lang="ts">
+const batiment = reactive({ latitutde: "loading", longitude: "loading" });
+const setBatiment = ({
+  latitude,
+  longitude,
+}: {
+  latitude: string;
+  longitude: string;
+}) => {
+  batiment.latitutde = latitude;
+  batiment.longitude = longitude;
+  console.debug("vue-route::from::", batiment.latitutde);
+};
+
+const retrieveBatiment = (id: string) => {
+  console.debug(id);
+};
+
+defineExpose({ setBatiment, retrieveBatiment });
 </script>
