dd1c3590a8a6acda94a23d0c7a45a1b200b47d22
JulioJu <11527844+JulioJu@users.noreply.github.com>
2022-11-01 00:40:59 +0100

    feat: home page add loader

--- src/map/MapContainer.vue
+++ src/map/MapContainer.vue
@@ -1,6 +1,10 @@
 <!-- Inspired from https://openlayers.org/en/latest/examples/popup.html -->
 <template>
   <section>
+    <div class="loading" v-if="loading">
+      <strong>Chargement des points</strong>
+      <div class="lds-hourglass"></div>
+    </div>
     <template v-if="isAuthenticated">
       <template v-if="isEditMode">
         <div class="text-info">
@@ -58,6 +62,8 @@ import Store from "@/store";
 
 const router = useRouter();
 
+const loading = ref(true);
+
 const isAuthenticated = Store.user.isAuthenticated;
 
 // https://openlayers.org/en/latest/doc/faq.html#why-is-my-map-centered-on-the-gulf-of-guinea-or-africa-the-ocean-null-island-
@@ -102,6 +108,7 @@ onMounted(async () => {
     popupContent: popupContent.value as HTMLDivElement,
     router,
   });
+  loading.value = false;
 });
 
 const toggleEditionMode = (): void => {
@@ -122,6 +129,50 @@ const toggleEditionMode = (): void => {
 </script>
 
 <style scoped="true" lang="scss">
+.loading {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 1000;
+  color: maroon;
+  .lds-hourglass {
+    /** https://loading.io/css/ */
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+    &:after {
+      content: " ";
+      display: block;
+      border-radius: 50%;
+      width: 0;
+      height: 0;
+      margin: 8px;
+      box-sizing: border-box;
+      border: 32px solid #fff;
+      border-color: #fff transparent #fff transparent;
+      animation: lds-hourglass 1.2s infinite;
+    }
+    @keyframes lds-hourglass {
+      0% {
+        transform: rotate(0);
+        animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+      }
+      50% {
+        transform: rotate(900deg);
+        animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+      }
+      100% {
+        transform: rotate(1800deg);
+      }
+    }
+  }
+}
+
 .map-container {
   width: 100%;
   height: 80vh;
